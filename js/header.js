6229f0adb69a4e281bc860a3bd95b6c84289e241
Aurora Piazza <apiazza-png@users.noreply.github.com>
2026-05-21 16:58:23 +0200

    feat: add About section with modal

--- js/header.js
+++ js/header.js
@@ -26,13 +26,12 @@
     var menu = document.createElement('div');
     menu.className = 'dropdown-menu';
 
-    var root = isLanding ? '' : '../';
     var projects = [
-      { name: 'Uova Strapazzate', path: root + 'RICETTA UOVA STRAPAZZATE/codebase/index.html' },
-      { name: 'Generazione di Pattern', path: root + 'PATTERNEXERCISE/index.html' },
-      { name: 'Tipografia Cinetica', path: root + 'TIPOGRAFIACINETICA/index.html' },
-      { name: 'Maschera Sonora', path: root + 'MASCHERASONORA/index.html' },
-      { name: 'Manionette', path: root + 'MANIONETTE/index.html' }
+      { name: 'Uova Strapazzate', path: 'RICETTA UOVA STRAPAZZATE/codebase/index.html' },
+      { name: 'Generazione di Pattern', path: 'PATTERNEXERCISE/index.html' },
+      { name: 'Tipografia Cinetica', path: 'TIPOGRAFIACINETICA/index.html' },
+      { name: 'Maschera Sonora', path: 'MASCHERASONORA/index.html' },
+      { name: 'Manionette', path: 'MANIONETTE/index.html' }
     ];
 
     projects.forEach(function(p) {
@@ -63,6 +62,60 @@
     navRight.appendChild(homeLink);
   }
 
+  var aboutBtn = document.createElement('button');
+  aboutBtn.className = 'about-btn';
+  aboutBtn.textContent = 'About';
+  navRight.appendChild(aboutBtn);
+
   header.appendChild(navRight);
   document.body.prepend(header);
+
+  // About modal
+  var modal = document.createElement('div');
+  modal.className = 'about-modal';
+
+  var modalContent = document.createElement('div');
+  modalContent.className = 'about-modal-content';
+
+  var closeBtn = document.createElement('button');
+  closeBtn.className = 'about-modal-close';
+  closeBtn.innerHTML = '&times;';
+  modalContent.appendChild(closeBtn);
+
+  var text = document.createElement('div');
+  text.className = 'about-modal-text';
+  text.innerHTML =
+    '<p>Sito sviluppato durante il workshop <strong>"No brain, No Gain"</strong>,<br>' +
+    'tenuto da <strong>whateverDev</strong> (Andrea Maffei &amp; Rocco Lorenzo Modugno),<br>' +
+    '18-22 Maggio 2026, all&rsquo;interno dei corsi:</p>' +
+    '<br>' +
+    '<p><strong>Interaction Design 2</strong><br>' +
+    'Prof. Alessia Scudieri</p>' +
+    '<br>' +
+    '<p><strong>Interaction Design 3</strong><br>' +
+    'Prof. Andrea Leonardi<br>' +
+    'Prof. Marcantonio D&rsquo;Antoni</p>' +
+    '<br>' +
+    '<p><strong>ABADIR</strong><br>' +
+    'Accademia di Design e Comunicazione Visiva<br>' +
+    'Sant&rsquo;Agata li Battiati CT<br>' +
+    'AA 2025-26</p>';
+  modalContent.appendChild(text);
+
+  modal.appendChild(modalContent);
+  document.body.appendChild(modal);
+
+  aboutBtn.addEventListener('click', function() {
+    modal.classList.add('open');
+  });
+
+  closeBtn.addEventListener('click', function() {
+    modal.classList.remove('open');
+  });
+
+  modal.addEventListener('click', function(e) {
+    if (e.target === modal) {
+      modal.classList.remove('open');
+    }
+  });
 })();
